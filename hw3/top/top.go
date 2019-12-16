6a4b41b2edf0d09e6f7d1a0c3f45a7b667d3cff2
Dementyev Alexandr <a-lex.demen@yandex.ru>
2019-12-16 21:52:08 +0300

    Добавлены сигнатуры методо

--- hw3/top/top.go
+++ hw3/top/top.go
@@ -2,15 +2,28 @@ package top
 
 import "strings"
 
-func top10(text string) []string{
+func top10(text string) []string {
 	return top(text, 10)
 }
 
-func top(text string, topCount int) []string{
-	return prepareText(text)
+func top(text string, topCount int) []string {
+	words := prepareText(text)
+
+	return words
 }
 
-func prepareText(text string) (res []string){
-	res = strings.Split(text, " ")
+func wordsCount(words []string) map[string]int {
+	res := make(map[string]int)
+
+	return res
+}
+
+func prepareText(text string) (res []string) {
+	words := strings.Split(strings.ToLower(text), " ")
+
+	for _, val := range words {
+		res = append(res, val)
+	}
+
 	return
 }
--- hw3/top/top_test.go
+++ hw3/top/top_test.go
@@ -2,11 +2,20 @@ package top
 
 import "testing"
 
-func TestPrepareText(t *testing.T){
-	testCases := []struct{
-		input string
+func TestPrepareText(t *testing.T) {
+	testCases := []struct {
+		input  string
 		output []string
 	}{
-		{"Test one - success.", []string{"test", "one", "success"}},
+		{"Test one - success.", []string{"test", "one", "-", "success"}},
+	}
+
+	var res []string
+	for _, testCase := range testCases {
+		res = prepareText(testCase.input)
+
+		if len(res) != len(testCase.output) {
+			t.Errorf("Тест завершился неудачей при входном значении - \"%s\"", testCase.input)
+		}
 	}
 }
