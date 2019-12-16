6d2de2be58d0ec100a5399a8a48c2b56df4eca09
a <a>
2019-12-16 23:09:12 +0300

    Логика

--- hw3/top/top.go
+++ hw3/top/top.go
@@ -1,29 +1,59 @@
 package top
 
-import "strings"
+import (
+	"sort"
+	"strings"
+)
+
+type wordStat struct{
+	word string
+	count int
+}
 
 func top10(text string) []string {
 	return top(text, 10)
 }
 
-func top(text string, topCount int) []string {
+func top(text string, topCount int) (res []string) {
 	words := prepareText(text)
+	wordCounter := wordsCount(words)
 
-	return words
+	var statistics []wordStat
+	for word, count := range wordCounter{
+		statistics = append(statistics, wordStat{word, count})
+	}
+
+	sort.Slice(statistics, func(i,j int) bool{
+		return statistics[i].count < statistics[j].count
+	})
+
+	for index, val := range statistics{
+		if index >= topCount {
+			break
+		}
+
+		res = append(res, val.word)
+	}
+
+	return
 }
 
 func wordsCount(words []string) map[string]int {
 	res := make(map[string]int)
+
+	for _, val := range words{
+		res[val]++
+	}
 
 	return res
 }
 
 func prepareText(text string) (res []string) {
 	words := strings.Split(strings.ToLower(text), " ")
-
-	for _, val := range words {
-		res = append(res, val)
-	}
+	res = words
+	//for _, val := range words {
+	//	res = append(res, val)
+	//}
 
 	return
 }
