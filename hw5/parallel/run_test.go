2f8cbf920300d94317a119a10da2f5ccbdecc497
a <a>
2020-01-04 15:07:24 +0300

    Дописаны тесты

--- hw5/parallel/run_test.go
+++ hw5/parallel/run_test.go
@@ -4,9 +4,10 @@ import (
 	"errors"
 	"sync/atomic"
 	"testing"
+	"time"
 )
 
-func TestHandleTask(t *testing.T){
+func TestHandleTask(t *testing.T) {
 	input := make(chan func() error)
 	output := make(chan error)
 	go handleTask(input, output)
@@ -15,7 +16,7 @@ func TestHandleTask(t *testing.T){
 		return nil
 	}
 
-	if err := <- output; err != nil{
+	if err := <-output; err != nil {
 		t.Errorf("Получено неожиоданное значение.")
 	}
 
@@ -23,8 +24,26 @@ func TestHandleTask(t *testing.T){
 		return errors.New("test")
 	}
 
-	if err := <- output; err == nil{
+	if err := <-output; err == nil {
 		t.Errorf("Получено неожиоданное значение.")
+	}
+}
+
+func TestSendTask(t *testing.T) {
+	var temp int
+	var isCompleted bool
+	taskChan := make(chan func() error)
+	task := func() error {
+		isCompleted = true
+		return nil
+	}
+	go sendTask(&temp, task, taskChan)
+
+	receivedTask := <-taskChan
+	receivedTask()
+
+	if !isCompleted {
+		t.Errorf("Задача не была выполнена.")
 	}
 }
 
@@ -40,25 +59,37 @@ func TestRun(t *testing.T) {
 
 	Run(tasks, 2, 1)
 
-	if len(tasks) != int(taskCompleted){
+	if len(tasks) != int(taskCompleted) {
 		t.Errorf("Неверное количество выполненных задач.")
 	}
 }
 
-func TestSendTask(t *testing.T){
-	var temp int
-	var isCompleted bool
-	taskChan := make(chan func() error)
+func TestRunWithError(t *testing.T) {
+	var taskCompleted uint32
+	var errorCount uint32
 	task := func() error {
-		isCompleted = true
+		atomic.AddUint32(&taskCompleted, 1)
 		return nil
 	}
-	go sendTask(&temp, task, taskChan)
-
-	receivedTask := <-taskChan
-	receivedTask()
-
-	if !isCompleted{
-		t.Errorf("Задача не была выполнена.")
+	errTask := func() error {
+		atomic.AddUint32(&taskCompleted, 1)
+		atomic.AddUint32(&errorCount, 1)
+		time.Sleep(1 * time.Millisecond)
+		return errors.New("")
 	}
+
+	tasks := []func() error{errTask, errTask, task, task, task, task, task}
+
+	N := 2
+	M := 1
+
+	Run(tasks, N, M)
+
+	if int(errorCount) != 2 {
+		t.Errorf("Неверное количество ошибок в выполненных задачах.")
+	}
+	if int(taskCompleted) > N+M {
+		t.Errorf("Количество выполненых задач превышает N+M=%d", N+M)
+	}
+
 }
