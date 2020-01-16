e2be555699bc32db82717e499c3208215cb2b571
a <a>
2020-01-16 07:00:39 +0300

    Цикл чтения

--- hw6/fcopy/copy.go
+++ hw6/fcopy/copy.go
@@ -7,20 +7,32 @@ func copy(src, dst string, offset, limit int) error {
 	return nil
 }
 
-func readFile(path string, offset int64, limit int) error {
+func readFile(path string, offset int64, limit int) (chan []byte, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer file.Close()
 
 	if offset > 0 {
 		_, err := file.Seek(offset, 0)
-		return err
+		return nil, err
 	}
-	limit := 1024 * 1024
-	for {
-		file.Read()
-	}
-	return nil
+
+	data := make(chan []byte)
+	go func() {
+		blockSize := 1024 * 1024
+		if limit != -1 && limit < blockSize {
+			blockSize = limit
+		}
+		dataBlock := make([]byte, 0, blockSize)
+		for {
+			file.Read(dataBlock)
+			select {
+			case data <- dataBlock:
+			}
+		}
+	}()
+
+	return data, nil
 }
