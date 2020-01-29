8a6fe6806805c89a7c1937141d3f298192a5d127
Dementyev Alexandr <a-lex.demen@yandex.ru>
2020-01-29 10:07:34 +0300

    executor

--- hw7/envdir/executor/executor.go
+++ hw7/envdir/executor/executor.go
@@ -7,7 +7,8 @@ import (
 )
 
 func RunCmd(cmd []string, env map[string]string) int {
-	command := exec.Command(cmd[0], cmd[1:len(cmd)-1]...)
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Env = prepareEnvVar(env)
 	err := command.Start()
 	if err != nil {
 		return 1
@@ -18,8 +19,16 @@ func RunCmd(cmd []string, env map[string]string) int {
 				return int(status.ExitCode)
 			}
 		} else {
-			log.Fatal("")
+			log.Fatal(err)
 		}
 	}
 	return 0
+}
+
+func prepareEnvVar(envs map[string]string) []string {
+	res := make([]string, len(envs))
+	for key, val := range envs {
+		res = append(res, key+"="+val)
+	}
+	return res
 }
--- hw7/envdir/main.go
+++ hw7/envdir/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/alexdemen/hw7/envdir/enviroment"
+	"github.com/alexdemen/hw7/envdir/executor"
+	"log"
+	"os"
+)
 
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatal("Неверное количество переданных элементов.")
+	}
+
+	envs, err := enviroment.ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(executor.RunCmd(os.Args[2:], envs))
 }
