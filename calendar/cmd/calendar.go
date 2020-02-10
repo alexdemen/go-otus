01e454c744b02c051f2abf01b8aec04c5a2df057
Dementyev Alexandr <a-lex.demen@yandex.ru>
2020-02-10 10:08:20 +0300

    Логирование

--- calendar/cmd/calendar.go
+++ calendar/cmd/calendar.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/alexdemen/go-otus/calendar/internal/config"
 	"github.com/alexdemen/go-otus/calendar/internal/middleware"
@@ -14,18 +15,29 @@ var configPath = flag.String("config", "", "path to configuration file")
 func init() {
 	flag.Parse()
 }
+
 func main() {
 	runConfig, err := config.GetConfiguration(*configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	middleware.ConfigureLogger("", middleware.Error)
+	level, err := chooseLoggerLevel(runConfig.LogLevel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = middleware.ConfigureLogger(runConfig.LogFile, level)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	handler := middleware.SetLogger(http.HandlerFunc(resolvePath))
 	if err := http.ListenAndServe(runConfig.ListenAddress, handler); err != nil {
 		fmt.Println(err)
 	}
+
+	middleware.CloseLogger()
 }
 
 func resolvePath(w http.ResponseWriter, r *http.Request) {
@@ -35,5 +47,20 @@ func resolvePath(w http.ResponseWriter, r *http.Request) {
 		return
 	default:
 		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func chooseLoggerLevel(levelName string) (int, error) {
+	switch levelName {
+	case "debug":
+		return middleware.Debug, nil
+	case "warn":
+		return middleware.Warning, nil
+	case "error":
+		return middleware.Error, nil
+	case "info":
+		return middleware.Info, nil
+	default:
+		return 0, errors.New("failed to select logger level")
 	}
 }
--- calendar/go.mod
+++ calendar/go.mod
@@ -5,10 +5,5 @@ go 1.13
 require (
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/pflag v1.0.5
-	go.uber.org/atomic v1.5.1 // indirect
-	go.uber.org/multierr v1.4.0 // indirect
-	go.uber.org/zap v1.13.0 // indirect
-	golang.org/x/lint v0.0.0-20200130185559-910be7a94367 // indirect
-	golang.org/x/tools v0.0.0-20200207224406-61798d64f025 // indirect
 	gopkg.in/yaml.v2 v2.2.8
 )
