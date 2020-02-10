f0e594887991d5bc94db07ae1bea435aec7311c9
Dementyev Alexandr <a-lex.demen@yandex.ru>
2020-02-10 10:50:10 +0300

    refact error file

--- calendar/internal/core/errors.go
+++ calendar/internal/core/errors.go
@@ -2,8 +2,8 @@ package core
 
 import "errors"
 
-var ErrEventNotExist = errors.New("event not exist")
-
-var ErrNoEvents = errors.New("no events")
-
-var ErrDateBusy = errors.New("this time is busy")
+var (
+	ErrEventNotExist = errors.New("event not exist")
+	ErrNoEvents      = errors.New("no events")
+	ErrDateBusy      = errors.New("this time is busy")
+)
